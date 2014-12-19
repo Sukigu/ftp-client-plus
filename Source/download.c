775307042c1531538ab90bad797096b37f4a0874
Tiago Neto <ei12109@fe.up.pt>
2014-12-19 15:31:43 +0000

    Final commit.

--- Source/download.c
+++ Source/download.c
@@ -1,6 +1,25 @@
 #include "../Headers/includes.h"
 #include "../Headers/user.h"
 
+typedef struct UserSettings {
+
+	// ID information
+	char* username;
+	char* password;
+
+	// Server Information
+	int port;
+	char* domain;
+	char* address;
+	char* path;
+	char* newIP;
+
+	// File info.
+	int pasv_port;
+	unsigned long fileSize;
+	char* filename;
+
+} Settings;
 
 static Settings *userSet;
 
@@ -15,18 +34,30 @@ int main(int argc, char** argv) {
     userSet->domain = malloc(255 * sizeof(char)+1);
     userSet->address = malloc(255 * sizeof(char)+1);
     userSet->filename = malloc(255 * sizeof(char)+1);
+    userSet->newIP = malloc(255 * sizeof(char)+1);
 
     if(argc != 2){
-        printf("\nERROR: %s ftp://[<user>:<password>@]<host>/<url-path>\n", argv[0]);
+        printf("\n> ERROR: %s ftp://[<user>:<password>@]<host>/<url-path>\n", argv[0]);
         exit(1);
     }
-    
+    else {
+    		clearScreen();
+    		printf("-------------------------------------------------\n");
+    		printf("-            WELCOME TO FTP-CLIENT               \n");
+    		printf("-------------------------------------------------\n");
+    	}
+
     fillUserSettings(argv[1]); 
-    //printf("%s\n%s\n%s\n%s\n%d", userSet->username, userSet->password, userSet->path, userSet->address,userSet->port);
     sockfd = createConnection(21);
     serverConversation(sockfd);
+    
+    printf("> Now exiting from server.\n");
+    char command[strlen("quit\n")];
+    strcpy(command,"quit\n");
+    send(sockfd, command, strlen(command), 0);
 
     close(sockfd);
+    printf("> Everything went ok. Exit successfully.\n");
     return 0;
 }
 
@@ -45,12 +76,12 @@ void fillUserSettings(char* url){
     char username[MAX_BUFFER_SIZE], password[MAX_BUFFER_SIZE], host[MAX_BUFFER_SIZE],path[MAX_BUFFER_SIZE];
 
     if ((i = sscanf(url, "ftp://%255[^:]:%255[^@]@%255[^/]/%s",username,password,host,path)) != 4 && (i = sscanf(url, "ftp://%255[^/]/%s",host,path)) != 2){
-        printf("\nERROR: The url you entered is not acceptable.\n");
+        printf("\n> ERROR: The url you entered is not acceptable.\n");
         exit(2);
     }
 
     if ((h=gethostbyname(host)) == NULL) {  
-            printf("We're sorry but either the server is not responding or this url is invalid.\n");
+            printf("> We're sorry but either the server is not responding or this url is invalid.\n");
             exit(3);
         }
 
@@ -62,13 +93,14 @@ void fillUserSettings(char* url){
     }
     else {
         strcpy(userSet->username,"anonymous");
-        strcpy(userSet->password,"-12345");
+        strcpy(userSet->password,"12345");
     }
     userSet->port = 21;
     strcpy(userSet->path, path);
     strcpy(userSet->address,inet_ntoa(*((struct in_addr *)h->h_addr)));
-    printf("%s\n",userSet->path);
-    //printf("%s\n%s\n%s\n%s\n%d", userSet->username, userSet->password, userSet->path, userSet->address,userSet->port);
+    printf(">  INITIAL IP ADDRESS : %s\n",userSet->address);
+    printf("> Username : %s\n",userSet->username);
+    printf("> Password : %s\n\n",userSet->password);
 }
 
 /**
@@ -80,10 +112,12 @@ int createConnection(int port){
     int sockfd;
     struct  sockaddr_in server_addr;
 
+
     /*server address handling*/
     bzero((char*)&server_addr,sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(userSet->address);       /*32 bit Internet address network byte ordered*/
+    if(port == 21) server_addr.sin_addr.s_addr = inet_addr(userSet->address);
+    else server_addr.sin_addr.s_addr = inet_addr(userSet->newIP);  /*32 bit Internet address network byte ordered*/
     server_addr.sin_port = htons(port);                  /*server TCP port must be network byte ordered */
     
     /*open an TCP socket*/
@@ -113,7 +147,7 @@ void serverConversation(int sockfd){
     if (response == 220) makeLogin(sockfd);
     //else if (response == 225) makeLogin(sockfd); // CHECK
     else {
-        printf("Something is wrong with the FTP server. The program will now exit!\n");
+        printf("> Something is wrong with the FTP server. The program will now exit!\n");
         exit(7);
     }
 }
@@ -144,16 +178,15 @@ void makeLogin(int sockfd){
             response = responseFromServer(sockfd, FALSE);
 
             if (response != 230){
-                printf("Login wasn't succefully ended! Sorry.\n");
+                printf("> Login wasn't succefully ended! Sorry.\n");
                 exit(9);
             }
             else {
-                printf("STARTING NOW THE TRANSFER OF FILE(S).\n");
                 handlingFiles(sockfd);
             }
         }
         else {
-            printf("Sorry. We tried to login but that didn't help. Got your username and password straight and try again.\n");
+            printf("> Sorry. We tried to login but that didn't help. Got your username and password straight and try again.\n");
             exit(8);
         }
 }
@@ -170,27 +203,23 @@ void handlingFiles(int sockfd){
         strcat(command,"\n");
 
         send(sockfd,command,strlen(command),0);
+
         answerGivenByServer = responseFromServer(sockfd,TRUE);
-        printf("Resposta: %d\n", answerGivenByServer);
+
         if (answerGivenByServer == 150)
         {
             get_file_name_from_path();
+            FILE* wantedFile;
+        	   printf("> STARTING NOW THE TRANSFER OF FILE '%s'.\n\n",userSet->filename);
+            wantedFile = fopen(userSet->filename, "wb");
+	    char buffer[128];
+	    int res = 1; 
+		while( (res = read(newSockFD,buffer,1)) > 0){
+			fwrite(buffer,1,1,wantedFile);
+	}
 
-            FILE* wantedFile;// = fopen(userSet->filename, "W");
-            wantedFile = fopen (userSet->filename, "wb");
-            char buffer[userSet->fileSize];
-            int controlo = -1;
-
-            int bytes_already_written = 0;
-            while( userSet->fileSize - bytes_already_written > 0){
-                while(controlo == -1)
-                    controlo = recv(newSockFD,buffer,userSet->fileSize,0);
-                if(controlo != -1){
-                    fwrite (buffer , sizeof(char), sizeof(buffer), wantedFile);
-                    bytes_already_written += strlen(buffer);
-                }
-            }
             fclose(wantedFile);
+            close(newSockFD);
         }
         else {
             printf("We're sorry. There was an error trying to access file. Try again.\n");
@@ -210,7 +239,7 @@ int parsingURLGivenByServer(int sockfd){
     while(receivedFromServer == -1)
         receivedFromServer = recv(sockfd,buffer,MAX_BUFFER_SIZE,0);
     buffer[receivedFromServer] = '\0';
-    printf("Resposta1: %s\n", buffer);
+    printf("> Resposta: %s\n", buffer);
 
     char temp[255];
     char AUX[2]; AUX[0] = '(';AUX[1] = ')';
@@ -237,18 +266,21 @@ int parsingURLGivenByServer(int sockfd){
         }
         else if (CONTROLO == END_OF_CYCLE) break;
     }
-    printf("Resposta2: %s\n", temp);
     // NO NEED TO CHANGE THE CODE BELOW.
 
     int parsing;
     // ip_1[3], ip_2[3], ip_3[3], ip_4[3]
     char ip_1[MAX_SERVER_RESPONSE_NUMBER_SIZE], ip_2[MAX_SERVER_RESPONSE_NUMBER_SIZE], ip_3[MAX_SERVER_RESPONSE_NUMBER_SIZE], ip_4[MAX_SERVER_RESPONSE_NUMBER_SIZE], port_1[MAX_SERVER_RESPONSE_NUMBER_SIZE], port_2[MAX_SERVER_RESPONSE_NUMBER_SIZE];
     if ((parsing = sscanf(temp, "(%4[^,],%4[^,],%4[^,],%4[^,],%4[^,],%4[^)]", ip_1, ip_2, ip_3, ip_4, port_1, port_2)) != 6){
-        printf("Server is not responding well. Try again again.\n");
+        printf("> Server is not responding well. Try again again.\n");
         exit(9);
     }
     else {
+        char newIP[MAX_BUFFER_SIZE];
+        sprintf (newIP, "%s.%s.%s.%s", ip_1, ip_2, ip_3, ip_4);
+        strcpy(userSet->newIP,newIP);
         userSet->pasv_port = 256*atoi(port_1) + atoi(port_2);
+        printf(">  NEW IP: '%s:%d'\n",userSet->newIP,userSet->pasv_port);
         return TRUE;
     }
 }
@@ -325,8 +357,18 @@ int responseFromServer(int sockfd, int getSize) {
     while(receivedFromServer == -1)
         receivedFromServer = recv(sockfd,buffer,MAX_BUFFER_SIZE,0);
     buffer[receivedFromServer] = '\0';
-            printf("Resposta: %s\n", buffer);
+            printf("> Resposta: %s", buffer);
     if (getSize == TRUE)    getFileSizeFromServerResponse(buffer);
 
     return atoi(buffer);
+}
+
+/**
+ * Clears the screen.
+ * @param  [NONE]
+ * @return [VOID]
+ */
+void clearScreen(){
+	int i = 0;
+	for(; i < 50; i++) {printf("\n");}
 }
